47f68223e591f3a8a466b5061b7f45af131bd279
Bhuvan Chennoju <36909603+bhuvan454@users.noreply.github.com>
2024-05-18 19:10:09 +0000

    changed reqeust to aiohttp and asyncio

--- crawler.py
+++ crawler.py
@@ -5,70 +5,27 @@ Bhuvan Chennoju
 '''
 import os
 import sys
-import json
 import asyncio
 from utils import *
 
-
 async def main():
-    args = get_args()
+    latest_year = await fetch_latest_conference_year()
+    args = get_args(latest_year)
+    check_args(args)
     start_year = args.start_year
     end_year = args.end_year
     output_dir = args.output_dir
-
-    #  parser.add_argument('--type', type=str, choices=['metadata', 'pdf', 'abstract', 'all'],
-    #                     default='all', help='Type of data to download (metadata, pdf, abstract, all)')
-
-    download_type = args.type 
+    download_type = args.type
+    verbose = args.verbose
 
     if not os.path.exists(output_dir):
         sys.exit(f"Output directory {output_dir} does not exist")
 
-
-    for year in range(start_year, end_year + 1):
-        year_dir = os.path.join(output_dir, str(year))
-
-        paper_ids, abstract_paths, metadata_paths, pdf_paths = await get_paper_paths(year)
-
-        for paper_id, abstract_path, metadata_path, pdf_path in zip(paper_ids, abstract_paths, metadata_paths, pdf_paths):
-            print(f"Downloading {paper_id}")
-            
-            # save directory for the paper
-            save_dir = os.path.join(year_dir, paper_id)
-            os.makedirs(save_dir, exist_ok=True)
-
-            if download_type == 'metadata':
-                metadata = get_metadata(metadata_path)
-                with open(os.path.join(save_dir, f"{paper_id}_metadata.json"), "w") as f:
-                    json.dump(metadata, f, indent=4)
-                continue
-
-            if download_type == 'pdf':
-                pdf = get_pdf(pdf_path)
-                with open(os.path.join(save_dir, f"{paper_id}.pdf"), "wb") as f:
-                    f.write(pdf)
-                continue
-
-            if download_type == 'abstract':
-                abstract = get_abstract(abstract_path)
-                with open(os.path.join(save_dir, f"{paper_id}_abstract.json"), "w") as f:
-                    json.dump(abstract, f, indent=4)
-                continue
-
-            if download_type == 'all':
-                abstract = get_abstract(abstract_path)
-                metadata = get_metadata(metadata_path)
-                pdf = get_pdf(pdf_path)
-                with open(os.path.join(save_dir, f"{paper_id}_abstract.json"), "w") as f:
-                    json.dump(abstract, f, indent=4)
-
-                with open(os.path.join(save_dir, f"{paper_id}_metadata.json"), "w") as f:
-                    json.dump(metadata, f, indent=4)
-
-                with open(os.path.join(save_dir, f"{paper_id}.pdf"), "wb") as f:
-                    f.write(pdf)
-                continue
-
+    async with aiohttp.ClientSession() as session:
+        tasks = [download_papers_for_year(session, year, output_dir, download_type,verbose= verbose) for year in range(start_year, end_year + 1)]
+        # if verbose:
+        #     tasks = tqdm(tasks, desc="Years")
+        await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- utils.py
+++ utils.py
@@ -1,20 +1,26 @@
+import os
 import sys
+import json
 import argparse
 import aiohttp
 import asyncio
-import requests
 from bs4 import BeautifulSoup
+from tqdm.asyncio import tqdm
 
 
-###  data fetch function
+
+
+# Data fetch function
 default_headers = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
 }
+
 
 base_url = "https://papers.nips.cc"
 
 
 
+############################################## Fetching Functions ##############################################################
 async def fetch(session, url):
     try:
         async with session.get(url, headers=default_headers) as response:
@@ -22,80 +28,71 @@ async def fetch(session, url):
             return await response.text()
     except aiohttp.ClientError as e:
         print(f"Error fetching {url}: {e}")
-        sys.exit(1)
+        return None
 
-async def fetch_all(session, urls):
-    tasks = [fetch(session, url) for url in urls]
-    return await asyncio.gather(*tasks)
+# async def fetch_all(session, urls):
+#     tasks = [fetch(session, url) for url in urls]
+#     return await asyncio.gather(*tasks)
 
 
-
-## helper functions
+############################################## Helper Functions ##############################################################
 def get_conference_url(year):
     return f"{base_url}/paper/{year}"
 
-
-def get_latest_conference_year():
+async def fetch_latest_conference_year():
     url = 'https://papers.nips.cc/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    async with aiohttp.ClientSession() as session:
+        page_content = await fetch(session, url)
+        if page_content is None:
+            return 2023
+        soup = BeautifulSoup(page_content, 'html.parser')
 
-    years = []
-    for item in soup.find_all('li'):
-        try:
-            year = int(item.text.split()[-1].strip('()'))
-            years.append(year)
-        except (ValueError, IndexError):
-            continue
+        years = []
+        for item in soup.find_all('li'):
+            try:
+                year = int(item.text.split()[-1].strip('()'))
+                years.append(year)
+            except (ValueError, IndexError):
+                continue
 
     return max(years) if years else 2023
 
 
-async def get_paper_paths(year):
+
+async def get_paper_paths(session, year):
     url = get_conference_url(year)
-    async with aiohttp.ClientSession() as session:
-        page_content = await fetch(session, url)
-        soup = BeautifulSoup(page_content, "html.parser")
-        
-        paper_ids, abstract_paths, metadata_paths, pdf_paths = [], [], [], []
+    page_content = await fetch(session, url)
+    if page_content is None:
+        return [], [], [], []
+    soup = BeautifulSoup(page_content, "html.parser")
     
+    paper_ids, abstract_paths, metadata_paths, pdf_paths = [], [], [], []
+
+    for li in soup.find("div", class_='container-fluid').find_all("li"):
+        paper_temp_url = li.a.get('href')
+        paper_id = paper_temp_url.split("/")[-1].split("-")[0]
         
-        for li in soup.find("div", class_='container-fluid').find_all("li"):
-            paper_temp_url = li.a.get('href')
-            paper_id = paper_temp_url.split("/")[-1].split("-")[0]
-            
-            paper_ids.append(paper_id)
-            abstract_paths.append(f"{base_url}{paper_temp_url}")
-            
-            paper_base_url = f"{base_url}{paper_temp_url.rsplit('.', 1)[0]}"
-            metadata_paths.append(f"{paper_base_url.replace('Abstract', 'Metadata').replace('hash', 'file')}.json")
-            pdf_paths.append(f"{paper_base_url.replace('Abstract', 'Paper').replace('hash', 'file')}.pdf")
+        paper_ids.append(paper_id)
+        abstract_paths.append(f"{base_url}{paper_temp_url}")
         
-        return paper_ids, abstract_paths, metadata_paths, pdf_paths
+        paper_base_url = f"{base_url}{paper_temp_url.rsplit('.', 1)[0]}"
+        metadata_paths.append(f"{paper_base_url.replace('Abstract', 'Metadata').replace('hash', 'file')}.json")
+        pdf_paths.append(f"{paper_base_url.replace('Abstract', 'Paper').replace('hash', 'file')}.pdf")
+    
+    return paper_ids, abstract_paths, metadata_paths, pdf_paths
 
 
-
-
-
-
-### argument parsing functions
-def get_args():
-    latest_year = get_latest_conference_year()
+######################################## Arguments and checks ################################################################
+def get_args(latest_year):
     parser = argparse.ArgumentParser(description='Download NIPS papers')
     parser.add_argument('--start_year', type=int, default=1987, help='start year')
     parser.add_argument('--end_year', type=int, default=latest_year, help='end year')
     parser.add_argument('--output_dir', type=str, default='data', help='output directory')
-
-    ## add arguments for downloading only metadata or pdfs or abstracts
-
-    parser.add_argument('--type', type=str, choices=['metadata', 'pdf', 'abstract', 'all'],
-                        default='all', help='Type of data to download (metadata, pdf, abstract, all)')
-    
-
+    parser.add_argument('--type', type=str, choices=['metadata', 'pdf', 'abstract', 'all'], default='all', help='Type of data to download (metadata, pdf, abstract, all)')
+    parser.add_argument('--verbose', action='store_true', help='verbose mode', default=False)
     return parser.parse_args()
 
 def check_args(args):
-
     if args.start_year < 1987:
         sys.exit("Start year must be 1987 or later")
     if args.start_year > args.end_year:
@@ -103,35 +100,76 @@ def check_args(args):
 
 
 
+############################################# Fetch, Save and Download Functions #############################################
+async def get_metadata(session, meta_path):
+    async with session.get(meta_path) as response:
+        if response.status_code != 200:
+            return {}
+        return await response.json()
 
-### data fetching functions 
-def get_metadata(meta_path):
-    response = requests.get(meta_path)
-    if response.status_code != 200:
-        return {}
-    return response.json()
-
-def get_pdf(pdf_path):
+async def get_pdf(session, pdf_path):
     try:
-        response = requests.get(pdf_path)
-        response.raise_for_status()
-        return response.content
+        async with session.get(pdf_path) as response:
+            response.raise_for_status()
+            return await response.read()
     except Exception as e:
         print(f"Error downloading PDF from {pdf_path}: {e}")
         return None
-    
 
-def get_abstract(abstract_path):
-    response = requests.get(abstract_path)
-    if response.status_code != 200:
-        return ""
-    soup = BeautifulSoup(response.text, "html.parser")
-    abstract_text = soup.find('h4', string ='Abstract').find_next('p').get_text()
+async def get_abstract(session, abstract_path):
+    async with session.get(abstract_path) as response:
+        if response.status_code != 200:
+            return ""
+        soup = BeautifulSoup(await response.text(), "html.parser")
+        abstract_text = soup.find('h4', string='Abstract').find_next('p').get_text()
+        abstract_text = " ".join(abstract_text.split())
+        title = soup.find('meta', {'name': 'citation_title'})['content']
+        authors = soup.find_all('meta', {'name': 'citation_author'})
+        authors = [author['content'] for author in authors]
+        return {'title': title, 'authors': authors, 'abstract': abstract_text}
 
-    # wrap the text into small paragraphs
-    abstract_text = " ".join(abstract_text.split())
-    
-    title = soup.find('meta', {'name': 'citation_title'})['content']
-    authors = soup.find_all('meta', {'name': 'citation_author'})
-    authors = [author['content'] for author in authors]
-    return {'title': title, 'authors': authors, 'abstract': abstract_text}
+
+############################################### Main functions ###############################################################
+
+
+async def download_papers_for_year(session, year, output_dir, download_type, verbose=False):
+    paper_ids, abstract_paths, metadata_paths, pdf_paths = await get_paper_paths(session, year)
+    # print(len(paper_ids), len(abstract_paths), len(metadata_paths), len(pdf_paths))
+
+    for paper_id, abstract_path, metadata_path, pdf_path in tqdm(zip(paper_ids, abstract_paths, metadata_paths, pdf_paths), desc=f"Year {year}"):
+
+        if verbose:
+            print(f"Downloading {paper_id} from {year}")
+
+        save_dir = os.path.join(output_dir, str(year), paper_id)
+        os.makedirs(save_dir, exist_ok=True)
+
+        if download_type in ['metadata', 'all']:
+            metadata = await get_metadata(session, metadata_path)
+            with open(os.path.join(save_dir, f"{paper_id}_metadata.json"), "w") as f:
+                json.dump(metadata, f, indent=4)
+        
+        if download_type in ['pdf', 'all']:
+            pdf = await get_pdf(session, pdf_path)
+            if pdf:
+                with open(os.path.join(save_dir, f"{paper_id}.pdf"), "wb") as f:
+                    f.write(pdf)
+        
+        if download_type in ['abstract', 'all']:
+            abstract = await get_abstract(session, abstract_path)
+            with open(os.path.join(save_dir, f"{paper_id}_abstract.json"), "w") as f:
+                json.dump(abstract, f, indent=4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
